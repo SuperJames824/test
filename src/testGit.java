aaf1af824d95635f97de7f815304800be7ffa596
SuperJames <767589019@qq.com>
2020-04-14 20:48:05 +0800

    提交新建项目

---
+++ src/testGit.java
@@ -0,0 +1,13 @@
+public class testGit {
+
+    public static void main(String[] args) {
+        System.out.println("测试git。。。。。。。。。。。");
+        System.out.println("测试git。。。。。。。。。。。");
+        System.out.println("测试git。。。。。。。。。。。");
+        System.out.println("测试git。。。。。。。。。。。");
+        System.out.println("测试git。。。。。。。。。。。");
+        System.out.println("测试git。。。。。。。。。。。");
+        System.out.println("测试git。。。。。。。。。。。");
+
+    }
+}
